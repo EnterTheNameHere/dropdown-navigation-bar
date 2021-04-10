440181a2e7eabbf41a44f811b8ea63198a913ce7
EnterTheNameHere Bohemian <email@enterthenamehere.com>
2021-04-10 12:34:56 +0200

    test: add module.exports expression to test files

---
+++ testFiles/javascript/export/module-exports.js
@@ -0,0 +1,12 @@
+
+module.exports = require('electron').clipboard;
+
+module.exports = async ({ test, benchmarkPaths }) => {};
+module.exports = () => {};
+module.exports = function() {};
+module.exports = function named() {};
+module.exports = function namedWithParam( param ) {};
+module.exports = function namedWithTwoParams( paramOne, paramTwo ) {};
+module.exports = async function namedAsync() {};
+module.exports = async function namedAsyncWithParam( param ) {};
+module.exports = async function namedAsyncWithTwoParams( paramOne, paramTwo ) {};
