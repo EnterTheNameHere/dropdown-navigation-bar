3cd7e69fdaa5c930003335c140241f1f3e8ee0a8
EnterTheNameHere Bohemian <email@enterthenamehere.com>
2021-04-20 23:38:47 +0200

    test: more test cases

--- testFiles/javascript/export/module-exports.js
+++ testFiles/javascript/export/module-exports.js
@@ -1,4 +1,12 @@
 
+module.exports = test;
+module.exports.one = test;
+module.exports.one.two = test;
+module.exports.one.two.three = test;
+module.exports.one.two.three['string'] = test;
+module.exports.one.two.three[value] = test;
+module.exports.one.two.three[()=>{return 0;}] = test;
+
 module.exports = require('electron').clipboard;
 
 module.exports = async () => {};
@@ -14,3 +22,67 @@ module.exports = function namedWithTwoParams( paramOne, paramTwo ) {};
 module.exports = async function namedAsync() {};
 module.exports = async function namedAsyncWithParam( param ) {};
 module.exports = async function namedAsyncWithTwoParams( paramOne, paramTwo ) {};
+
+module.exports = function*() {};
+module.exports = function* namedGenerator() {};
+module.exports = function* namedGeneratorWithParam( param ) {};
+module.exports = function* namedGeneratorWithTwoParams( paramOne, paramTwo ) {};
+module.exports = async function* namedGeneratorAsync() {};
+module.exports = async function* namedGeneratorAsyncWithParam( param ) {};
+module.exports = async function* namedGeneratorAsyncWithTwoParams( paramOne, paramTwo ) {};
+
+module.exports.STATE_NONE = TrackPlayer.STATE_NONE;
+module.exports.STATE_READY = TrackPlayer.STATE_READY;
+module.exports.STATE_PLAYING = TrackPlayer.STATE_PLAYING;
+
+module.exports.setupPlayer = setupPlayer;
+module.exports.destroy = TrackPlayer.destroy;
+
+module.exports.TrackPlayerEvents = require('./eventTypes');
+module.exports.useTrackPlayerEvents = require('./hooks').useTrackPlayerEvents;
+
+var KalturaAppTokenStatus = module.exports.KalturaAppTokenStatus = {
+    DISABLED : 1,
+    ACTIVE : 2,
+    DELETED : 3,
+};
+
+module.exports.base_url     = '/api/:version';
+module.exports.node_port    = 8080;
+module.exports.longerExports.evenLongerExports = something;
+
+module.exports = {
+    generateCustomKey: function(size, callback) {
+        callback(rs.generate(size))
+    },
+    generateID: function(callback) {
+    }
+}
+
+module.exports =
+    (function(modules) {
+    })
+    ({});
+    
+module.exports['add'] = _add;
+module.exports['addproject'] = _addproject;
+
+module.exports = class ClassName {
+    classPropertyNumber = 42;
+    classPropertyString = "string";
+    classPropertyArrowFunction = () => {};
+
+    constructor() {}
+
+    async memberAsyncMethod() {}
+    async *memberAsyncGeneratorMethod() {}
+    memberMethod() {}
+    *memberGeneratorMethod() {}
+
+    propertyArrowAsyncMethod = async () => {}
+    propertyArrowMethod = () => {};
+    propertyFunctionAsyncMethod = async function() {}
+    propertyFunctionAsyncGeneratorMethod = async function*() {}
+    propertyFunctionMethod = function() {}
+    propertyFunctionGeneratorMethod = function*() {}
+}
